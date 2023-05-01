1b0f2d414ee35b4681f6690f2a8e216a6abc1577
Adolfo Herrera <valherdolf@gmail.com>
2023-05-01 18:51:17 -0400

    Refactor file name and variable names for better readability

---
+++ src/data/gameboard-image-levels.js
@@ -0,0 +1,17 @@
+import GameBoardImageLevel1 from '../assets/gameboard/gameboard-level-1.jpg';
+import GameBoardImageLevel2 from '../assets/gameboard/gameboard-level-2.jpg';
+import GameBoardImageLevel3 from '../assets/gameboard/gameboard-level-3.jpg';
+import GameBoardImageLevel4 from '../assets/gameboard/gameboard-level-4.jpg';
+import GameBoardImageLevel5 from '../assets/gameboard/gameboard-level-5.jpg';
+import GameBoardImageLevel6 from '../assets/gameboard/gameboard-level-6.jpg';
+
+const GAMEBOARD_IMAGE_LEVELS = {
+  LEVEL1: GameBoardImageLevel1,
+  LEVEL2: GameBoardImageLevel2,
+  LEVEL3: GameBoardImageLevel3,
+  LEVEL4: GameBoardImageLevel4,
+  LEVEL5: GameBoardImageLevel5,
+  LEVEL6: GameBoardImageLevel6,
+};
+
+export default GAMEBOARD_IMAGE_LEVELS;
