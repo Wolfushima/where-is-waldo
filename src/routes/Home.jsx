bfc808df9f83487c9e20ed256c6a9a2e5b008f47
Adolfo Herrera <valherdolf@gmail.com>
2023-05-02 15:07:07 -0400

    Added grid of level images to Home Page.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Wrapper from '../components/Wrapper';
+import GAMEBOARD_IMAGE_LEVELS from '../data/gameboard-image-levels';
 
 export default function Home() {
-  return <Link to="game/LEVEL1">LEVEL1</Link>;
+  return (
+    <div id="home" className="home">
+      <Wrapper className="home">
+        <section className="home__game-levels">
+          {Object.entries(GAMEBOARD_IMAGE_LEVELS).map(([key, value]) => {
+            return (
+              <Link to={`game/${key}`} className="home__game-level">
+                <p>{value.level}</p>
+                <div className="home__game-img">
+                  <img src={value.src} alt="Gameboard" />
+                </div>
+              </Link>
+            );
+          })}
+        </section>
+      </Wrapper>
+    </div>
+  );
 }
