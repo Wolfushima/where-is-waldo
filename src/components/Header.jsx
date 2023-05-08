ee0440eec87f562689aaafea6845aea97e32b1c6
Adolfo Herrera <valherdolf@gmail.com>
2023-05-08 19:01:26 -0400

    Update Header component style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Wrapper from './Wrapper';
+import WaldoImg from '../assets/characters/waldo.png';
 
 const Header = () => (
   <header id="header" className="header">
     <Wrapper className="header">
+      <Link to="/" className="header__icon-waldo">
+        <img src={WaldoImg} alt="Waldo" />
+      </Link>
       <h1>
-        WHERE&apos;S <span>WALDO?</span>
+        <Link to="/">
+          WHERE&apos;S&nbsp;<span>WALDO?</span>
+        </Link>
       </h1>
     </Wrapper>
   </header>
