1234f2b495a4f4c0afee000847b8ef21cc4b8e1b
Adolfo Herrera <valherdolf@gmail.com>
2023-05-08 20:26:41 -0400

    Add section to Home page, this section explains objective and gameplay

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,6 +7,28 @@ export default function Home() {
   return (
     <div id="home" className="home">
       <Wrapper className="home">
+        <section className="home__game-description">
+          <h2>
+            Welcome to &quot;Where&apos;s Waldo?&quot; - the photo tagging game
+            that will test your observation skills!
+          </h2>
+          <p className="bold">Objective and Gameplay</p>
+          <p>
+            <span className="red">&#x2022;</span> In this game application,
+            there are 6 different levels of increasing difficulty, each with a
+            large photograph that contains Waldo, Wizard, Wilma, and Odlaw.
+          </p>
+          <p>
+            <span className="red">&#x2022;</span> Your task is to locate each of
+            these characters by clicking on the corresponding area of the photo
+            and selecting the correct name from a list of possible options.
+          </p>
+          <p>
+            <span className="red">&#x2022;</span> Once you find all characters
+            in a level, you can register your highscore and move on to the next
+            level.
+          </p>
+        </section>
         <section className="home__game-levels">
           {Object.entries(GAMEBOARD_IMAGE_LEVELS).map(([key, value]) => {
             return (
