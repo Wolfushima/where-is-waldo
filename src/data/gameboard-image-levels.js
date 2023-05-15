591202e3d47bb8e51f4aaf3fb0b0eeed86c951ab
Adolfo Herrera <valherdolf@gmail.com>
2023-05-15 16:13:11 -0400

    Add levelNumber property to GAMEBOARD_IMAGE_LEVELS levels.

--- src/data/gameboard-image-levels.js
+++ src/data/gameboard-image-levels.js
@@ -9,26 +9,32 @@ const GAMEBOARD_IMAGE_LEVELS = {
   LEVEL1: {
     src: GameBoardImageLevel1,
     level: 'Level 1',
+    levelNumber: '1',
   },
   LEVEL2: {
     src: GameBoardImageLevel2,
     level: 'Level 2',
+    levelNumber: '2',
   },
   LEVEL3: {
     src: GameBoardImageLevel3,
     level: 'Level 3',
+    levelNumber: '3',
   },
   LEVEL4: {
     src: GameBoardImageLevel4,
     level: 'Level 4',
+    levelNumber: '4',
   },
   LEVEL5: {
     src: GameBoardImageLevel5,
     level: 'Level 5',
+    levelNumber: '5',
   },
   LEVEL6: {
     src: GameBoardImageLevel6,
     level: 'Level 6',
+    levelNumber: '6',
   },
 };
 
