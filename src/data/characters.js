632bb64529e033ce5eb37c3d81023c5dfd07ce23
Adolfo Herrera <valherdolf@gmail.com>
2023-05-23 16:53:46 -0400

    Add id to CHARACTERS.

--- src/data/characters.js
+++ src/data/characters.js
@@ -4,10 +4,10 @@ import WizardImg from '../assets/characters/wizard.png';
 import OdlawImg from '../assets/characters/odlaw.png';
 
 const CHARACTERS = [
-  { src: WaldoImg, alt: 'Waldo' },
-  { src: WandaImg, alt: 'Wanda' },
-  { src: WizardImg, alt: 'Wizard' },
-  { src: OdlawImg, alt: 'Odlaw' },
+  { src: WaldoImg, alt: 'Waldo', id: 'WALDO_' },
+  { src: WandaImg, alt: 'Wanda', id: 'WANDA_' },
+  { src: WizardImg, alt: 'Wizard', id: 'WIZARD_' },
+  { src: OdlawImg, alt: 'Odlaw', id: 'ODLAW_' },
 ];
 
 export default CHARACTERS;
