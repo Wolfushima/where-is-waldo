bba4c5d9b25c73921255045c2e203dbe7ebe656f
Adolfo Herrera <valherdolf@gmail.com>
2023-05-24 20:30:29 -0400

    feat: Implement character selection with targeting box in GameBoardBoard component and perform refactoring

--- src/components/gameboard/GameBoard.jsx
+++ src/components/gameboard/GameBoard.jsx
@@ -1,65 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { formatISO } from 'date-fns';
-import moveMagnifier from '../../utils/gameboard/move-magnifier';
-import getCursorPos from '../../utils/gameboard/get-cursor-pos';
 import Wrapper from '../Wrapper';
 import GameBoardBoard from './GameBoardBoard';
-import {
-  setCharacterStatus,
-  setGameOver,
-  setDate,
-} from '../../slices/gameboard/currentGameSlice';
+import { setGameOver, setDate } from '../../slices/gameboard/currentGameSlice';
 import GameBoardOverview from './GameBoardOverview';
 import GameBoardFooter from './GameBoardFooter';
 import GameBoardModal from './GameBoardModal';
 
 const GameBoard = ({ boardImg, level }) => {
-  const [isZoomEnabled, setIsZoomEnabled] = useState(false);
-  const [magnifierGlassStyle, setMagnifierGlassStyle] = useState({
-    backgroundImage: `url(${boardImg})`,
-  });
-  const { CHARACTERS_POSITION_BOUNDS } = useSelector(
-    (state) => state.levels[level]
-  );
-  const { CHARACTERS_STATUS, CHARACTERS, GAME_OVER } = useSelector(
+  const { CHARACTERS_STATUS, GAME_OVER } = useSelector(
     (state) => state.currentGame[level]
   );
-  const dispatch = useDispatch();
+  const [isZoomEnabled, setIsZoomEnabled] = useState(false);
   const [startGame, setStartGame] = useState(false);
-
-  const handleBoardImgZoom = (e) => {
-    if (isZoomEnabled) {
-      moveMagnifier(e, magnifierGlassStyle, setMagnifierGlassStyle);
-    }
-  };
-
-  const handleBoardImgClick = (e) => {
-    const imgElement = e.target;
-    const { cursorX, cursorY, imgRect } = getCursorPos(e, imgElement);
-
-    const relativePosX = Math.floor((cursorX / imgRect.width) * 100);
-    const relativePosY = Math.floor((cursorY / imgRect.height) * 100);
-
-    const isCharacterLocated = (posX, posY, characterName) => {
-      const characterData = CHARACTERS_POSITION_BOUNDS[characterName];
-      const isFound =
-        posX > characterData.xLeft &&
-        posX < characterData.xRight &&
-        posY > characterData.yTop &&
-        posY < characterData.yBottom;
-      if (isFound && !CHARACTERS_STATUS[characterName].isFound) {
-        dispatch(setCharacterStatus({ level, character: characterName }));
-        console.log(`You found ${characterName}!`);
-      }
-    };
-
-    CHARACTERS.forEach((character) => {
-      if (!CHARACTERS_STATUS[character].isFound) {
-        isCharacterLocated(relativePosX, relativePosY, character);
-      }
-    });
-  };
+  const dispatch = useDispatch();
 
   const toggleBoardImgZoom = () => {
     setIsZoomEnabled(!isZoomEnabled);
@@ -90,9 +45,7 @@ const GameBoard = ({ boardImg, level }) => {
             <GameBoardBoard
               isZoomEnabled={isZoomEnabled}
               boardImg={boardImg}
-              handleBoardImgZoom={handleBoardImgZoom}
-              handleBoardImgClick={handleBoardImgClick}
-              magnifierGlassStyle={magnifierGlassStyle}
+              level={level}
             />
             <GameBoardFooter
               toggleBoardImgZoom={toggleBoardImgZoom}
