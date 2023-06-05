5bae87cd6ff186ca62a7c5d3dd4bf8e12226e727
Adolfo Herrera <valherdolf@gmail.com>
2023-06-05 19:25:14 -0400

    feat: Add <span> element to anchor to create underline effect

--- src/components/home/HomeLeaderboard.jsx
+++ src/components/home/HomeLeaderboard.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 export default function HomeLeaderboard() {
   return (
     <section className="home__leaderboard">
-      <Link to="leaderboard/LEVEL1">VIEW LEADERBOARD</Link>
+      <Link to="leaderboard/LEVEL1">
+        VIEW LEADERBOARD
+        <span className="anchor-underline" />
+      </Link>
     </section>
   );
 }
