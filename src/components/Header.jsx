12abdf36193dcd17c9263891cb2e5488c90c8c8a
Adolfo Herrera <valherdolf@gmail.com>
2023-06-06 18:44:56 -0400

    feat: Add navbar to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Wrapper from './Wrapper';
 import WaldoImg from '../assets/characters/waldo.png';
 
-const Header = () => (
-  <header id="header" className="header">
-    <Wrapper className="header">
-      <Link to="/" className="header__icon-waldo">
-        <img src={WaldoImg} alt="Waldo" />
-      </Link>
-      <h1>
-        <Link to="/">
-          WHERE&apos;S&nbsp;<span>WALDO?</span>
-        </Link>
-      </h1>
-    </Wrapper>
-  </header>
-);
+const Header = () => {
+  return (
+    <header id="header" className="header">
+      <Wrapper className="header">
+        <div className="header__logo">
+          <Link to="/" className="header__icon-waldo">
+            <img src={WaldoImg} alt="Waldo" />
+          </Link>
+          <h1>
+            <Link to="/">
+              WHERE&apos;S&nbsp;<span>WALDO?</span>
+            </Link>
+          </h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="leaderboard/LEVEL1">Leaderboard</NavLink>
+            </li>
+            <li>
+              <NavLink to="about">About</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Wrapper>
+    </header>
+  );
+};
 
 export default Header;
